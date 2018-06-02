12e1c96062069455ab7ce661a6da5cece9f3aa6e
Ivo-Donchev <ivodonchev267@gmail.com>
2018-06-02 15:34:09 +0300

    Add check if import exists

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import snake
 
 import os
+import re
 import glob
 
 
@@ -14,9 +15,25 @@ DECLARATIONS = (
     ARROW_FUNCTION_DECLARATION
 )
 
+IMPORT_PATTERN = "import (?:[\"'\s]*([\w*{}\n, ]+) from \s*)?[\"'\s]*(([\.]+)?[@\w\/_-]+)[\"'\s]*;?"
+
 @snake.key_map("<leader>D")
 def react_dummy_goto_def():
     word = snake.get_word()
+
+    current_filename = snake.get_current_file()
+    with open(current_filename) as current_file:
+        content = current_file.read()
+        imports = re.findall(IMPORT_PATTERN, content)
+
+        import_found = False
+        for imp in imports:
+            if any([word in imp_word for imp_word in imp]):
+                import_found = True
+
+
+        if not import_found:
+            print('import not found :(')
 
     for file in glob.glob('src/**/*.js', recursive=True):
         with open(file) as f:
